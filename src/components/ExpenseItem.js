9e9b757160d1c4e73b8f2c0d13d39110bc297aaf
Kenyon Huppe <kenyonhuppe@gmail.com>
2021-11-15 11:25:59 -0800

    Outsourced Date to Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,18 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = ({ expenseItem }) => {
-    const month = expenseItem.date.toLocaleString('en-US', { month: 'long' });
-    const day = expenseItem.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = expenseItem.date.getFullYear();
+
 
 
     return (
         <div className='expense-item'>
 
 
-            <div>
-                <div className='date month'>{month}</div>
-                <div className='date day'>{day}</div>
-                <div className='date year'>{year}</div>
-            </div>
+            <ExpenseDate expenseDate={expenseItem.date} />
             <div className='expense-item__description'>
                 <h2>{expenseItem.title}</h2>
-                <div className='expense-item__price'>$ {expenseItem.amount}</div>
+                <div className='expense-item__price'>${expenseItem.amount}</div>
             </div>
 
         </div>
